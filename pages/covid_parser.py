9fe8d734834497bf84adb3d19c4fabc89c343835
shehel <shehel@live.com>
2020-07-17 18:16:27 +0300

    Daily updates

--- pages/covid_parser.py
+++ pages/covid_parser.py
@@ -27,7 +27,7 @@ def FileCheck(fn):
 #Parser to get latest metrics as soon as they are published
 def get_covid_metrics_parser():
   user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-  headers={'User-Agent':user_agent,} 
+  headers={'User-Agent':user_agent,}
   url = "https://www.moph.gov.qa/english/Pages/Coronavirus2019.aspx"
   required_elem = {'Total Number of People Tested':0, }
   #get already saved data
@@ -35,7 +35,7 @@ def get_covid_metrics_parser():
   df_max_date = df.Date.iloc[-1]#datetime.strptime(df.Date.max(), '%Y-%m-%d')
   print (df_max_date)
   covid_cases={}
-  # covid_cases['new_cases']         = "" 
+  # covid_cases['new_cases']         = ""
   # covid_cases['new_deaths']        = ""
   # covid_cases['new_recovered']     = ""
   # covid_cases['recovered_cases']   = ""
@@ -44,7 +44,7 @@ def get_covid_metrics_parser():
   # covid_cases['total_tested']      = ""
   # covid_cases['total_positive']    = ""
   # covid_cases['icu_cases']         = ""
-  # covid_cases['acute_care_cases']  = "" 
+  # covid_cases['acute_care_cases']  = ""
 
 
   #Open page and parse html
@@ -54,7 +54,7 @@ def get_covid_metrics_parser():
       #Read HTTP response
       source = response.read()
       soup = BeautifulSoup(source, 'html.parser')
-      #Get latest update date, see if it in our records 
+      #Get latest update date, see if it in our records
       date_elem = soup.find('div', class_='col-7 col-lg-10 text-left')
       date_elem = date_elem.text.replace(u'\xa0', u' ')
       date_elem = date_elem.split(" ")[1:]
@@ -88,7 +88,7 @@ def get_covid_metrics_parser():
               #   covid_cases['recovered_cases']      = stat_number
               # elif (elem == "Deaths"):
               #   covid_cases['death_cases']      = stat_number
-            
+
         #print(covid_cases)
         #Fields to save
         cols =['Date', 'Number of New Positive Cases in Last 24 Hrs', 'Total Number of Positive Cases to Date',
@@ -96,11 +96,11 @@ def get_covid_metrics_parser():
                 'Total Number of Active Cases Undergoing Treatment to Date','Number of New Recovered Cases in Last 24 Hrs',
                 'Total Number of Recovered Cases to Date','Number of New Deaths in Last 24 Hrs',
                 'Total Number of Deaths to Date']
-  
+
         # df2 = pd.DataFrame(list(zip('', covid_cases['new_cases'],covid_cases['total_positive'],
-        #       "NA", covid_cases['total_tested'] , covid_cases['active_cases'], "NA", covid_cases['recovered_cases'], 
-        #       "NA", covid_cases['death_cases'] 
-        #   )), columns = cols) 
+        #       "NA", covid_cases['total_tested'] , covid_cases['active_cases'], "NA", covid_cases['recovered_cases'],
+        #       "NA", covid_cases['death_cases']
+        #   )), columns = cols)
         df2 = pd.DataFrame([covid_cases])
         datetime_object = datetime.strftime(datetime_object, '%Y-%m-%d')
         df2['Date'] = datetime_object
@@ -114,26 +114,26 @@ def get_covid_metrics_parser():
         df.to_csv('data/covid_data.csv', index=None, encoding='utf-8')
 
 
- 
+
 
 
 #Parser written to get covid data from data.gov.qa
-# %% 
+# %%
 '''
-The order of retrieval of data from MoPH is 
+The order of retrieval of data from MoPH is
 Total tested, Total positive,
 Number of active, cases in last 24,
 currently under acute care, currently in icu,
-recovered patients, deaths. This is the order 
+recovered patients, deaths. This is the order
 data is stored in new_stats
 '''
 def get_covid_metrics_model():
 
-  if not FileCheck("../data/covid_data.csv"):
+  if not FileCheck("./data/covid_data.csv"):
     request.urlretrieve("https://www.data.gov.qa/explore/dataset/covid-19-cases-in-qatar/download/?format=csv&timezone=Asia/Baghdad&lang=en&use_labels_for_header=true&csv_separator=%2C", "../data/covid_data.csv")
     df = pd.read_csv('./data/covid_data.csv', sep=',')
     df = df.fillna(0)
-    for col in df.columns[1:]: 
+    for col in df.columns[1:]:
         df[col]=df[col].astype(int)
     df['Date'] =pd.to_datetime(df.Date)
     df = df.sort_values(by='Date')
@@ -141,7 +141,7 @@ def get_covid_metrics_model():
     df.to_csv('./data/covid_data.csv', index=None)
   else:
     print ("Exists")
-    
+
     df = pd.read_csv('./data/covid_data.csv')
     prev = list(df.iloc[-1])
     repeat = True
@@ -174,29 +174,29 @@ def get_covid_metrics_model():
       # for i in soup.find_all("h3", class_="my-2"):
       #   try:
       #       new_stats.append(int(i.find("strong").string))
-      #   except: 
+      #   except:
       #       new_stats.append(int(i.find("b").string))
 
 
-      
-      # If new entry added, 
+
+      # If new entry added,
       if prev[4] == new_stats[0]:
         # Wait 1 minutes before trying again
         time.sleep(60)
-        continue    
+        continue
       else:
         current_date = datetime.datetime.strptime(prev[0], "%Y-%m-%d")
         new_date = current_date + datetime.timedelta(days=1)
-        
+
         # Append new entries
         df.loc[len(df)] = [new_date.strftime("%Y-%m-%d"),new_stats[3],new_stats[1],
-                          new_stats[0]-prev[4], new_stats[0], prev[5]+new_stats[3]-(new_stats[6]-prev[7]), 
-                          new_stats[6]-prev[7], new_stats[6], new_stats[-1]-prev[-1], 
+                          new_stats[0]-prev[4], new_stats[0], prev[5]+new_stats[3]-(new_stats[6]-prev[7]),
+                          new_stats[6]-prev[7], new_stats[6], new_stats[-1]-prev[-1],
                           new_stats[-1]]
         print ("Dataframe appended and written to disk.")
         df.to_csv('./data/covid_data.csv', index=None)
         repeat = False
-      
+
 
 
 get_covid_metrics_model()
